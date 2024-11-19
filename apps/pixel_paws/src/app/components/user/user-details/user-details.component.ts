6b38dce8f0421a8bae247bd8fa32468787a1863e
not101Xander <xander380@gmail.com>
2024-11-19 19:21:02 +0100

    added user components

---
+++ apps/pixel_paws/src/app/components/user/user-details/user-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-details.component.html',
+  styleUrl: './user-details.component.css',
+})
+export class UserDetailsComponent {}
