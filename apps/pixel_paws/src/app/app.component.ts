edfd8733186fa7ae292d34f2b2013d748871c278
not101Xander <xander380@gmail.com>
2024-11-19 23:48:19 +0100

    ui improvements

--- apps/pixel_paws/src/app/app.component.ts
+++ apps/pixel_paws/src/app/app.component.ts
@@ -4,14 +4,27 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { HeaderComponent } from './components/ui/header/header.component';
 import { FooterComponent } from './components/ui/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrimeNGConfig } from 'primeng/api';
+import { FieldsetModule } from 'primeng/fieldset';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, HeaderComponent, FooterComponent, RouterOutlet],
+  imports: [NxWelcomeComponent, RouterModule, HeaderComponent, FooterComponent, RouterOutlet, FieldsetModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pixel_paws';
+  constructor(private primengConfig: PrimeNGConfig) {
+    this.primengConfig.ripple = true;
+    this.primengConfig.zIndex = {
+      modal: 1100,    // dialog, sidebar
+      overlay: 1000,  // dropdown, overlaypanel
+      menu: 1000,     // overlay menus
+      tooltip: 1100   // tooltip
+  };
+  
+  }
 }
