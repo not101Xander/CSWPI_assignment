daa6edc0fe319364fcf7d984d56cb3cf2dba3c76
not101Xander <xander380@gmail.com>
2024-11-19 19:34:50 +0100

    added about page

--- apps/pixel_paws/src/app/app.routes.ts
+++ apps/pixel_paws/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
+import { UserDetailsComponent } from './components/user/user-details/user-details.component';
+import { UserListComponent } from './components/user/user-list/user-list.component';
+import { AboutPageComponent } from './components/about/about.component';
 
 export const appRoutes: Route[] = [];
